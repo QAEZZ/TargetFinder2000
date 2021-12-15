f0ea10613208657b300431a48d5d84e1c4737ee1
Russian-Dev <65181690+Russian-Dev@users.noreply.github.com>
2021-12-15 17:06:12 -0600

    Update main.py

--- Windows (CMD)/main.py
+++ Windows (CMD)/main.py
@@ -35,7 +35,7 @@ def main():
     
     asciiArt = ['sus.py', 'trol.py', 'gobl.py', 'hak.py', 'babyRick.py', 'ZAMN.py', 'cry.py', 'hi.py', 'bing.py', 'cred.py', 'gloc.py', 'heli.py', 'mo.py', 'roc.py', 'wall.py', '?.py', 'noo.py', 'trolSmile.py', 'fsociety.py']
 
-    os.system("python ./asciis/" + random.choice(asciiArt))
+    os.system("python3 ./asciis/" + random.choice(asciiArt))
 
     typeOption = int(input('[$]>>: '))
 
