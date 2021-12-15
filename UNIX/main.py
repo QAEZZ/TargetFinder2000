fd0107d9fda1d48bc807ec98b581878e00364cdb
Russian-Dev <65181690+Russian-Dev@users.noreply.github.com>
2021-12-15 17:06:52 -0600

    Update main.py

--- UNIX/main.py
+++ UNIX/main.py
@@ -8,14 +8,14 @@ def main():
 
     asciiArt = ['sus.py', 'trol.py', 'gobl.py', 'hak.py', 'babyRick.py', 'ZAMN.py', 'cry.py', 'hi.py', 'bing.py', 'cred.py', 'gloc.py', 'heli.py', 'mo.py', 'roc.py', 'wall.py', '?.py', 'noo.py', 'trolSmile.py', 'fsociety.py']
 
-    os.system("python ./asciis/" + random.choice(asciiArt))
+    os.system("python3 ./asciis/" + random.choice(asciiArt))
 
     typeOption = int(input('[$]>>: '))
 
     if typeOption == 1:
-      os.system('python home.py')
+      os.system('python3 home.py')
     elif typeOption == 2:
-      os.system('python business.py')
+      os.system('python3 business.py')
     elif typeOption == 3:
       print('TERMINATED!..')
       exit()
